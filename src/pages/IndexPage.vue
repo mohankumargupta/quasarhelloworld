f6f7fbe099109d49edf78d42a5ffea51ef6cf236
Mohan Gupta <mohangupta@live.com>
2025-03-04 21:44:35 +1100

    codemirror

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,6 +1,17 @@
 <script setup lang="ts">
 import { ref } from 'vue';
+import  CodeMirror from 'vue-codemirror6';
+import { javascript } from '@codemirror/lang-javascript';
 
+const code = ref(
+`const difference = (a, b) => {
+  const s = new Set(b);
+  return a.filter(x => !s.has(x));
+};
+
+difference([1, 2, 3], [1, 2, 4]); // [3]
+`);
+const extensions = ref([javascript()]);
 const splitter = ref(50);
 </script>
 
@@ -15,7 +26,13 @@ const splitter = ref(50);
       left
     </template>
     <template #after>
-      right
+      <code-mirror
+        v-model="code"
+        disabled
+        :extensions="extensions"
+        basic
+        wrap
+      />
     </template>
   </q-splitter>
 </template>
