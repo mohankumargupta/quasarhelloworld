ceb620eb6e718869ace98a04cfacf51adce856d4
Raspberry Pi <raspberrypisig@gmail.com>
2025-03-05 18:27:29 +1100

    improving

--- src/components/BlocklyViewer.vue
+++ src/components/BlocklyViewer.vue
@@ -3,20 +3,22 @@
   import * as Blockly from 'blockly/core';
   import 'blockly/blocks';
   import type { Workspace } from 'blockly/core'; // Import types
-  type BlocklyOptions = {
-    toolbox?: HTMLElement | string;
-    readOnly?: boolean;
-    scrollbars?: boolean;
-    options?: {
-      toolbox?: HTMLElement | string;
-      readOnly?: boolean;
-      scrollbars?: boolean;
-    };
-  };
+  import { toolbox } from './toolbox';
 
-  const props = defineProps<BlocklyOptions>();
+  // type BlocklyOptions = {
+  //   toolbox?: HTMLElement | string;
+  //   readOnly?: boolean;
+  //   scrollbars?: boolean;
+  //   options?: {
+  //     toolbox?: HTMLElement | string;
+  //     readOnly?: boolean;
+  //     scrollbars?: boolean;
+  //   };
+  // };
 
-  const blocklyToolbox = ref();
+  // const props = defineProps<BlocklyOptions>();
+
+  // const blocklyToolbox = ref();
   const blocklyDiv = ref();
   const workspace = shallowRef<Workspace>();
 
@@ -24,12 +26,8 @@
 
   onMounted(() => {
     Blockly.setLocale({locale: 'en'});
-    //Blockly.setLocale(En);
-    const options: BlocklyOptions = props.options || {};
-    if (!options.toolbox) {
-      options.toolbox = blocklyToolbox.value;
-    }
-    workspace.value = Blockly.inject(blocklyDiv.value, options);
+    const zoom = {controls: true};
+    workspace.value = Blockly.inject(blocklyDiv.value, {toolbox, zoom});
   });
   </script>
 
